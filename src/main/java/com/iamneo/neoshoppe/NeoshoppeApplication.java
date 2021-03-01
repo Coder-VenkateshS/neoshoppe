adf61fbdc1baaba216320ca6214b3d9e9fda39eb
FACEPrep-ProGrad <prograd@faceprep.in>
2021-03-01 18:05:38 +0530

    initial code

---
+++ src/main/java/com/iamneo/neoshoppe/NeoshoppeApplication.java
@@ -0,0 +1,14 @@
+package com.iamneo.neoshoppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoshoppeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Spring application started");
+		SpringApplication.run(NeoshoppeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamneo/neoshoppe/NeoshoppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamneo.neoshoppe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoshoppeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
